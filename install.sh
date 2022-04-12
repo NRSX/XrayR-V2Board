5c533ef8bb1c83b8becb532662a45e06c40de89f
NRSX <79076114+NRSX@users.noreply.github.com>
2022-04-12 19:57:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -200,7 +200,7 @@ install_XrayR() {
 
     # Writing json
     echo "正在尝试写入配置文件..."
-    wget https://cdn.jsdelivr.net/gh/missuo/XrayR-V2Board/config.yml -O /etc/XrayR/config.yml
+    wget https://cdn.jsdelivr.net/gh/NRSX/XrayR-V2Board/config.yml -O /etc/XrayR/config.yml
     sed -i "s/NodeID:.*/NodeID: ${node_id}/g" /etc/XrayR/config.yml
     sed -i "s/NodeType:.*/NodeType: ${node_type}/g" /etc/XrayR/config.yml
     echo ""
@@ -241,8 +241,7 @@ install_XrayR() {
     echo "XrayR version            - 查看 XrayR 版本"
     echo "------------------------------------------"
     echo "One-Step Script Based on XrayR-Release"
-    echo "Telegram: https://t.me/missuo"
-    echo "Github: https://github.com/missuo/XrayR-V2Board"
+    echo "Github: https://github.com/NRSX/XrayR-V2Board"
     echo "Powered by Vincent"
 }
 
